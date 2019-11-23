95654d3f300df8b5bf86c4ebde3e0548b929285d
daniel.deliu2@ing.com <daniel.deliu2@ing.com>
2019-11-24 00:39:15 +0200

    implement getproductinformation method v5

--- src/main/java/com/orangejuice/SmartShoppingAPI/product/resources/ProductResource.java
+++ src/main/java/com/orangejuice/SmartShoppingAPI/product/resources/ProductResource.java
@@ -1,22 +1,14 @@
 package com.orangejuice.SmartShoppingAPI.product.resources;
 
-import com.orangejuice.SmartShoppingAPI.product.service.ProductInformation;
 import com.orangejuice.SmartShoppingAPI.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.util.List;
 
 @RestController
 public class ProductResource {
