8923b329c182ebdbb14c9dedaef660bdc665bffa
daniel.deliu2@ing.com <daniel.deliu2@ing.com>
2019-11-24 00:38:11 +0200

    implement getproductinformation method v4

--- src/main/java/com/orangejuice/SmartShoppingAPI/product/resources/ProductResource.java
+++ src/main/java/com/orangejuice/SmartShoppingAPI/product/resources/ProductResource.java
@@ -5,6 +5,7 @@ import com.orangejuice.SmartShoppingAPI.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +33,7 @@ public class ProductResource {
     }
 
     @GetMapping("/{prodname}")
-    public String getProductInformation(@RequestParam(name = "prodname", required = false, defaultValue = "lapte") String productName) {
+    public String getProductInformation(@PathVariable(name = "prodname", required = false) String productName) {
         try {
             return productService.getProductInformation(productName);
         } catch (KeyStoreException e) {
