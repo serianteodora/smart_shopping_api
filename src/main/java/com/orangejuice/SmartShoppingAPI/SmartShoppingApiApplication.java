228841fc2b29d4e8985e93d9d789d1af1e73eed7
Teo <serian.teodora@gmail.com>
2019-11-23 14:05:12 +0200

    First commit

---
+++ src/main/java/com/orangejuice/SmartShoppingAPI/SmartShoppingApiApplication.java
@@ -0,0 +1,13 @@
+package com.orangejuice.SmartShoppingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartShoppingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartShoppingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orangejuice/SmartShoppingAPI/SmartShoppingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orangejuice.SmartShoppingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartShoppingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
