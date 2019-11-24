53856241925dfbd9db755d5aeda6da0354bab9bf
daniel.deliu2@ing.com <daniel.deliu2@ing.com>
2019-11-24 09:32:22 +0200

    implement getproductinformation method v7

--- src/main/java/com/orangejuice/SmartShoppingAPI/product/resources/ProductResource.java
+++ src/main/java/com/orangejuice/SmartShoppingAPI/product/resources/ProductResource.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.ws.rs.PathParam;
 import java.util.Map;
 
 @RestController
